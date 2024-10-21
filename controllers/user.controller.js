b95e04c232dec520de50deddda0dbd96d534a8f2
rabosian <msann1356@gmail.com>
2024-10-20 23:55:43 -0400

    signup error handler updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,18 +6,22 @@ const userController = {};
 
 userController.signup = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, role } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400), json({ error: "user already exists" });
+      throw new Error("user already exists");
+    }
+
+    if (!password || password.trim() === "") {
+      throw new Error("password cannot be empty")
     }
 
     const hash = await bcrypt.hash(password, saltRounds);
-    const newUser = new User({ name, email, password: hash });
+    const newUser = new User({ name, email, password: hash, role });
     await newUser.save();
     res.status(201).json({ status: "success" });
   } catch (err) {
-    res.status(500).json({ status: "failed", error: err.message });
+    res.status(400).json({ status: "failed", error: err.message });
   }
 };
 
--- routes/users.api.js
+++ routes/users.api.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const userController = require("../controllers/user.controller");
 const router = express.Router();
+const userController = require("../controllers/user.controller");
 
 router.post("/signup", userController.signup);
 
